48c930c5ed3c2ceef178bf273356e13050de84a9
aws <awsalomari11469@gmail.com>
2023-11-02 10:18:13 +0300

    lab 28

--- app/src/main/java/com/asac26/taskmaster/MainActivity.java
+++ app/src/main/java/com/asac26/taskmaster/MainActivity.java
@@ -1,72 +1,101 @@
 package com.asac26.taskmaster;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-/** @noinspection ALL*/
+import com.asac26.taskmaster.adapter.TasksRecyclerViewAdapter;
+import com.asac26.taskmaster.models.Task;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    SharedPreferences sp;
-    public static final String TASK_TAG="taskName";
-
+    @SuppressLint({"MissingInflatedId", "SetTextI18n"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sp = PreferenceManager.getDefaultSharedPreferences(this);
 
-        ImageButton userProfile = (ImageButton) findViewById(R.id.userProfile);
-        userProfile.setOnClickListener(view -> {
-            Intent goToUserSettings = new Intent(MainActivity.this, UserSettingsActivity.class);
-            startActivity(goToUserSettings);
-        });
-
-        Button task1 = findViewById(R.id.task1);
-        task1.setOnClickListener(view -> {
-            Intent goToTaskDetails = new Intent(MainActivity.this, TaskDetailsActivity.class);
-            goToTaskDetails.putExtra(TASK_TAG,task1.getText().toString());
-            startActivity(goToTaskDetails);
-        });
-        Button task2 = findViewById(R.id.task2);
-        task2.setOnClickListener(view -> {
-            Intent goToTaskDetails = new Intent(MainActivity.this, TaskDetailsActivity.class);
-            goToTaskDetails.putExtra(TASK_TAG,task2.getText().toString());
-            startActivity(goToTaskDetails);
-        });
-        Button task3 = findViewById(R.id.task3);
-        task3.setOnClickListener(view -> {
-            Intent goToTaskDetails = new Intent(MainActivity.this, TaskDetailsActivity.class);
-            goToTaskDetails.putExtra(TASK_TAG,task3.getText().toString());
-            startActivity(goToTaskDetails);
-        });
-
-        Button addTaskButton = findViewById(R.id.addTaskHome);
+        Button addTaskButton= findViewById(R.id.addTaskHome);
         addTaskButton.setOnClickListener(view -> {
-            System.out.println("Add Task Button Clicked");
-            Intent goToNewTaskFormIntent = new Intent(MainActivity.this, AddTasksActivity.class);
-            startActivity(goToNewTaskFormIntent);
+            Intent goToAddTaskFormIntent = new Intent(MainActivity.this, AddTasksActivity.class);
+            startActivity(goToAddTaskFormIntent);
         });
-        Button allTasksButton = findViewById(R.id.allTasksHome);
-        allTasksButton.setOnClickListener(view -> {
-            Intent goToAllTasksFormIntent = new Intent(MainActivity.this, AllTasksActivity.class);
-            startActivity(goToAllTasksFormIntent);
-        });
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        TextView userTasks;
-        userTasks = findViewById(R.id.userName);
-        String name = sp.getString(UserSettingsActivity.USERNAME_TAG, "no name");
-        userTasks.setText(name.isEmpty() ? "tasks" : name + "'s tasks");
+        //===============================================================
+
+        Button allTaskButton=findViewById(R.id.allTasksHome);
+        allTaskButton.setOnClickListener(view -> {
+            Intent goToAllTasksIntent = new Intent(MainActivity.this, AllTasksActivity.class);
+            startActivity(goToAllTasksIntent);
+        });
+
+        //================================================================
+
+        ImageButton settingsPage=findViewById(R.id.userProfile);
+        settingsPage.setOnClickListener(view -> {
+            Intent goToSettings=new Intent(MainActivity.this,UserSettingsActivity.class);
+            startActivity(goToSettings);
+        });
+
+        //=================================================================
+
+        TextView user=findViewById(R.id.userName);
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("username", "DefaultUsername");
+        user.setText(username +"'s Tasks:");
+
+        //==================================================================
+
+        Button firstTask=findViewById(R.id.task1);
+        firstTask.setOnClickListener(view -> {
+            Intent sendTitle=new Intent(MainActivity.this,TaskDetailsActivity.class);
+            sendTitle.putExtra("taskTitle","Cleaning");
+            startActivity(sendTitle);
+        });
+
+
+        Button secondTask=findViewById(R.id.task2);
+        secondTask.setOnClickListener(view -> {
+            Intent sendTitle=new Intent(MainActivity.this,TaskDetailsActivity.class);
+            sendTitle.putExtra("taskTitle","Studying");
+            startActivity(sendTitle);
+        });
+
+
+        Button thirdTask=findViewById(R.id.task3);
+        thirdTask.setOnClickListener(view -> {
+            Intent sendTitle=new Intent(MainActivity.this,TaskDetailsActivity.class);
+            sendTitle.putExtra("taskTitle","Playing");
+            startActivity(sendTitle);
+        });
+
+        //==========================================================================================
+
+        RecyclerView recyclerView = findViewById(R.id.recycleView);
+        TasksRecyclerViewAdapter taskAdapter = new TasksRecyclerViewAdapter(Collections.singletonList((Task) createSampleTasks()), this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(taskAdapter);
+
+    }
+    private List<Task> createSampleTasks() {
+        List<Task> tasks = new ArrayList<>();
+        tasks.add(new Task("Chores", "Washing the dishes", "new"));
+        tasks.add(new Task("Entertainment", "Listening to music", "assigned"));
+        tasks.add(new Task("Groceries", "Buy bread", "in progress"));
+        return tasks;
     }
 }
