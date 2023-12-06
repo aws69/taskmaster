e22ee7fbc0e84fb46e2856eba13fb5b78d138e89
aws <awsalomari11469@gmail.com>
2023-12-06 19:44:18 +0300

    lab36

---
+++ app/src/main/java/com/asac26/taskmaster/activities/LogInActivity.java
@@ -0,0 +1,56 @@
+package com.asac26.taskmaster.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.amplifyframework.core.Amplify;
+import com.asac26.taskmaster.R;
+
+public class LogInActivity extends AppCompatActivity {
+    public static final String TAG="LoginActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+
+        loginButton();
+        signUpButton();
+    }
+
+    public void loginButton(){
+        Intent callingIntent=getIntent();
+        String email=callingIntent.getStringExtra(VerifyAccountActivity.VERIFY_EMAIL_TAG);
+        EditText usernameEditText= findViewById(R.id.editTextTextEmailAddressVerfy);
+        usernameEditText.setText(email);
+
+        //===========================================================
+        Button loginButton=findViewById(R.id.verify);
+        loginButton.setOnClickListener(v->{
+            String username=usernameEditText.getText().toString();
+            String password=((EditText)findViewById(R.id.editTextTextPasswordSignUp)).getText().toString();
+            Amplify.Auth.signIn(username,password,
+                    success->{
+                        Log.i(TAG,"login successfully"+ success);
+                        Intent goToHomeIntent=new Intent(LogInActivity.this,MainActivity.class);
+                        startActivity(goToHomeIntent);
+                    },
+                    failure->{Log.i(TAG,"login failed"+ failure);
+                        Toast.makeText(LogInActivity.this,"Login Failed",Toast.LENGTH_LONG).show();
+                    });
+        });
+    }
+
+    public void signUpButton(){
+        Button signupButton= findViewById(R.id.goSignUp);
+        signupButton.setOnClickListener(view -> {
+            Intent goToSignUp = new Intent(LogInActivity.this, SignUpActivity.class);
+            startActivity(goToSignUp);
+        });
+    }
+}
--- app/src/main/java/com/asac26/taskmaster/activities/MainActivity.java
+++ app/src/main/java/com/asac26/taskmaster/activities/MainActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -41,11 +42,13 @@ public class MainActivity extends AppCompatActivity {
         amplifier();
         setUpTaskListRecyclerView();
         queryTasks();
-        AddTaskButton();
-        AllTasksButton();
-        SettingsButton();
+        setAddTaskButton();
+        setAllTasksButton();
+        setSettingsButton();
+        setLoginAndLogOutButton();
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onResume() {
         super.onResume();
@@ -55,23 +58,23 @@ public class MainActivity extends AppCompatActivity {
         user.setText(username +"'s Tasks:");
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     public void amplifier(){
         Amplify.API.query(
                 ModelQuery.list(Task.class),
                 success->{
                     Log.i(TAG,"Read tasks successfully");
                     tasks.clear();
-                    for (Task databaseTask : success.getData()) {;
+                    for (Task databaseTask : success.getData()) {
                         tasks.add(databaseTask);
                     }
-                    runOnUiThread(() -> {
-                        taskAdapter.notifyDataSetChanged();
-                    });
+                    runOnUiThread(() -> taskAdapter.notifyDataSetChanged());
                 },
                 failure-> Log.i(TAG,"failed to read tasks")
         );
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     private void queryTasks() {
         Amplify.API.query(
                 ModelQuery.list(Task.class),
@@ -84,9 +87,7 @@ public class MainActivity extends AppCompatActivity {
                             tasks.add(databaseTask);
                         }
                     }
-                    runOnUiThread(() -> {
-                        taskAdapter.notifyDataSetChanged();
-                    });
+                    runOnUiThread(() -> taskAdapter.notifyDataSetChanged());
                 },
                 failure -> Log.i(TAG, "Couldn't read tasks from DynamoDB ")
         );
@@ -94,7 +95,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void setUpTaskListRecyclerView(){
-        RecyclerView taskListRecycleReview = (RecyclerView) findViewById(R.id.recycleView);
+        RecyclerView taskListRecycleReview = findViewById(R.id.recycleView);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         taskListRecycleReview.setLayoutManager(layoutManager);
         taskAdapter = new TasksRecyclerViewAdapter(tasks, this);
@@ -102,15 +103,15 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void AddTaskButton() {
-        Button addTaskButton = findViewById(R.id.addTaskButton);
+    private void setAddTaskButton() {
+        Button addTaskButton = findViewById(R.id.addTaskHome);
         addTaskButton.setOnClickListener(view -> {
             Intent goToAddTaskFormIntent = new Intent(MainActivity.this, AddTasksActivity.class);
             startActivity(goToAddTaskFormIntent);
         });
     }
 
-    private void AllTasksButton() {
+    private void setAllTasksButton() {
         Button allTaskButton = findViewById(R.id.allTasksHome);
         allTaskButton.setOnClickListener(view -> {
             Intent goToAllTasksIntent = new Intent(MainActivity.this, AllTasksActivity.class);
@@ -119,11 +120,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void SettingsButton() {
+    private void setSettingsButton() {
         ImageButton settingsPage = findViewById(R.id.userProfile);
         settingsPage.setOnClickListener(view -> {
             Intent goToSettings = new Intent(MainActivity.this, UserSettingsActivity.class);
             startActivity(goToSettings);
         });
+    }
+
+    public void setLoginAndLogOutButton(){
+        Button loginButton=findViewById(R.id.buttonLogIn);
+        loginButton.setOnClickListener(v->{
+            Intent goToLogin = new Intent(MainActivity.this, LogInActivity.class);
+            startActivity(goToLogin);
+        });
+
+        Button logoutButton=findViewById(R.id.buttonLogOut);
+        logoutButton.setOnClickListener(v-> Amplify.Auth.signOut(
+                ()-> {Log.i(TAG,"logOut successfully");
+                    runOnUiThread(()-> ((TextView)findViewById(R.id.userNikName)).setText(""));
+                    Intent goToLogin = new Intent(MainActivity.this, LogInActivity.class);
+                    startActivity(goToLogin);
+                },
+                failure->{Log.i(TAG,"logOut failed");
+                    runOnUiThread(()-> Toast.makeText(MainActivity.this,"Logout Failed",Toast.LENGTH_LONG));
+                }));
     }
 }
